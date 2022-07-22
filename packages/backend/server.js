e023ba7e06a9c05a5ded90ba049776c0aeb19bda
KrishnaCD93 <krishna.duvvuri93@gmail.com>
2022-07-22 13:45:02 -0600

    cors

--- packages/backend/server.js
+++ packages/backend/server.js
@@ -17,12 +17,18 @@ const server = new ApolloServer({
 async function startApolloServer(server) {
   await server.start();
   const corsOptions = {
-    origin: true,
-    // [
-    //   "https://cookbook.social",
-    //   "https://studio.apollographql.com",
-    //   "http://localhost:3000"
-    // ],
+    origin: (origin, callback) => {
+      const whitelist = [
+        "https://cookbook.social",
+        "https://studio.apollographql.com",
+        "http://localhost:3000"
+      ]
+      if (whitelist.indexOf(origin) !== -1) {
+        callback(null, true)
+      } else {
+        callback(new Error("Not allowed by CORS"))
+      }
+    },
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true,
   }
